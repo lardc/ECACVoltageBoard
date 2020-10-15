ec15d8788c15c80dd073967d5aaf7987d3019d56
asytyi <alexander.a.sytyi@gmail.com>
2020-10-15 12:52:42 +0300

    Выпилены мёртвые функции из LowLevel

--- Firmware/Source/Controller/LowLevel.c
+++ Firmware/Source/Controller/LowLevel.c
@@ -23,53 +23,29 @@ void LL_SetSync2State(bool NewState)
 	GPIO_SetState(GPIO_CTRL_SYNC_2, NewState);
 }
 //-----------------------------
+
 void LL_ConnectPOWRelay(bool NewState)
 {
 	GPIO_SetState(GPIO_POW_RELAY, NewState);
 }
-
 //-----------------------------
+
 void LL_ConnectCTRLRelay(bool NewState)
 {
 	GPIO_SetState(GPIO_CTRL_RELAY, NewState);
 }
-
 //-----------------------------
+
 void LL_EnablePWMOut()
 {
 	GPIO_SetState(GPIO_CTRL_PWMSD_1, true);
 	GPIO_SetState(GPIO_CTRL_PWMSD_2, true);
 }
-
 //-----------------------------
+
 void LL_DisablePWMOut()
 {
 	GPIO_SetState(GPIO_CTRL_PWMSD_1, false);
 	GPIO_SetState(GPIO_CTRL_PWMSD_2, false);
-}
-
-//-----------------------------
-
-bool LL_GetStateKU()
-{
-	return GPIO_GetState(GPIO_U_RANGE);
-}
-//-----------------------------
-
-bool LL_GetStateKI_H()
-{
-	return GPIO_GetState(GPIO_I_RANGE_H);
-}
-//-----------------------------
-
-bool LL_GetStateKI_M()
-{
-	return GPIO_GetState(GPIO_I_RANGE_M);
-}
-//-----------------------------
-
-bool LL_GetStateKI_L()
-{
-	return GPIO_GetState(GPIO_I_RANGE_L);
 }
 //-----------------------------
--- Firmware/Source/Controller/LowLevel.h
+++ Firmware/Source/Controller/LowLevel.h
@@ -12,9 +12,5 @@ void LL_ConnectPOWRelay(bool NewState);
 void LL_ConnectCTRLRelay(bool NewState);
 void LL_EnablePWMOut();
 void LL_DisablePWMOut();
-bool LL_GetStateKU();
-bool LL_GetStateKI_H();
-bool LL_GetStateKI_M();
-bool LL_GetStateKI_L();
 
 #endif //__LOWLEVEL_H
