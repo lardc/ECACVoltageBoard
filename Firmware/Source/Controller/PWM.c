228d18345c6ffa8d5cc8fafe6392fcdfdc111a00
asytyi <alexander.a.sytyi@gmail.com>
2020-10-16 13:49:41 +0300

    Реализованы функции получения текущего напряжения и пересчёта его в ШИМ

--- Firmware/Source/Controller/PWM.c
+++ Firmware/Source/Controller/PWM.c
@@ -3,20 +3,21 @@
 
 // Includes
 #include "Measure.h"
-#include "Global.h"
 #include "Timer1PWM.h"
 #include "math.h"
 #include "DeviceObjectDictionary.h"
 #include "DataTable.h"
-#include "Board.h"
+#include "SysConfig.h"
 
 // Variables
 volatile uint32_t PWMTimerCounter = 0;
-static float TargetValue, Kp, Ki, ErrorI;
+static float ActualAmplitude, TargetAmplitude, AmplitudeLimit, AmplitudeRiseStep, Kp, Ki, ErrorI;
 static float TransformerRatio;
 
 // Forward functions
 void PWM_CacheParameters();
+float PWM_GetInstantVoltageSetpoint();
+float PWM_ConvertVoltageToPWM(float Voltage);
 
 // Functions
 void PWM_SignalStart(uint16_t Voltage, uint32_t Current)
@@ -34,23 +35,43 @@ void PWM_SignalStop()
 
 void PWM_SinRegulation()
 {
-	float VoltageSetpoint = TargetValue * sinf(2 * M_PI * PWMTimerCounter / PWM_SINE_COUNTER_MAX);
+	// Алгоритм нарастания уставки напряжения
+	if(PWMTimerCounter == 0 && ActualAmplitude < TargetAmplitude)
+	{
+		ActualAmplitude += AmplitudeRiseStep;
+		if(ActualAmplitude > TargetAmplitude)
+			ActualAmplitude = TargetAmplitude;
+	}
+
+	// Запись уставки ШИМ
+	float InstantVoltage = PWM_GetInstantVoltageSetpoint();
+	float PWMSetpoint = PWM_ConvertVoltageToPWM(InstantVoltage);
+	T1PWM_SetDutyCycle(PWMSetpoint);
 }
 //------------------------------------------------
 
-void PWM_ApplyControl(float Voltage)
+float PWM_GetInstantVoltageSetpoint()
 {
+	return ActualAmplitude * sinf(2 * M_PI * PWMTimerCounter / PWM_SINE_COUNTER_MAX);
+}
+//------------------------------------------------
 
+float PWM_ConvertVoltageToPWM(float Voltage)
+{
+	return Voltage / TransformerRatio / PWM_PRIMARY_VOLTAGE * T1PWM_GetPWMBase();
 }
 //------------------------------------------------
 
 void PWM_CacheParameters()
 {
 	ErrorI = 0;
-
-	TransformerRatio = (float)DataTable[REG_PWM_TRANS_RATIO] / 100;
-
 	Kp = (float)DataTable[REG_KP] / 1000;
 	Ki = (float)DataTable[REG_KI] / 1000;
+
+	TargetAmplitude = (float)DataTable[REG_VOLTAGE_SETPOINT] * M_SQRT2;
+
+	TransformerRatio = (float)DataTable[REG_PWM_TRANS_RATIO] / 100;
+	AmplitudeLimit = (float)DataTable[REG_PWM_OUT_VOLTAGE_LIMIT];
+	AmplitudeRiseStep = (float)DataTable[REG_PWM_VOLTAGE_RISE_RATE] / PWM_SINE_FREQ;
 }
 //------------------------------------------------
